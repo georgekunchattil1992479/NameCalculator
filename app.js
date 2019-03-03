12002c8f7c83dbc6d1b06fc89866c6f623edf5b6
George Jacob <georgekunchattil@gmail.com>
2019-03-03 21:14:55 +0530

    ignore node_modules

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-var student = {
-  name: "",
-  type: "student"
-};
+(function () {
+  'use strict';
 
-document.addEventListener('DOMContentLoaded', contentLoaded);
+  angular.module('NameCalculator', [])
 
-function contentLoaded(event) {
-  document.getElementById('name').addEventListener("keyup", keyUp);
-}
+  .controller('NameCalculatorController', function ($scope) {
+      /* $scope.name = "Yaakov";
+      $scope.sayHello = function () {
+        return "Hello Coursera!";  
+      }; */
+  });
 
-function keyUp(event) {
-  calculateNumericOutput();
-}
-
-function calculateNumericOutput() {
-  student.name = document.getElementById('name').value;
-
-  var totalNameValue = 0;
-  for (var i = 0; i < student.name.length; i++) {
-    totalNameValue += student.name.charCodeAt(i);
-  }
-
-  // Insert result into page
-  var output = "Total Numeric value of person's name is " + totalNameValue;
-  document.getElementById('output').innerText = output;
-}
+}) ();
