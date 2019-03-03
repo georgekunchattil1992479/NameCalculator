aaad5e6c7fb6ea3eb661508c66d6df1c04da1c8d
George Jacob <georgekunchattil@gmail.com>
2019-03-03 22:26:52 +0530

    Added controller 'NameCalculatorController', added function for calculating total of ascii value of each character which is typed

--- app.js
+++ app.js
@@ -6,6 +6,21 @@
   .controller('NameCalculatorController', function ($scope) {
       $scope.name = "";
       $scope.totalValue = 0;
+
+      $scope.displayNumeric = function () {
+          var totalNameValue = calculatNumericForString($scope.name);  //get the total Value
+          $scope.totalValue = totalNameValue;
+      };
+
+      function calculatNumericForString(string) {
+          var totalStringValue = 0;
+          for (var i = 0; i < string.length; i++) {
+              totalStringValue += string.charCodeAt(i);
+          }
+
+          return totalStringValue;
+      }
+
   });
 
 }) ();
