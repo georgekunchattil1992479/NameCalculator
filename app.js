ce0ca38f5d887e5e922d285e0d98e882c7e90f27
George Jacob <georgekunchattil@gmail.com>
2019-03-03 21:51:17 +0530

    Added scope variables

--- app.js
+++ app.js
@@ -4,10 +4,8 @@
   angular.module('NameCalculator', [])
 
   .controller('NameCalculatorController', function ($scope) {
-      /* $scope.name = "Yaakov";
-      $scope.sayHello = function () {
-        return "Hello Coursera!";  
-      }; */
+      $scope.name = "";
+      $scope.totalValue = 0;
   });
 
 }) ();
